3e66873ed9f170fe62d446a84e0d52fc4dc6a6a3
Augusto Madeira Splett (Tinho) <augusto.splett@gmail.com>
2022-01-06 18:23:24 -0300

    criacao da pasta helper e classe de operações

---
+++ Colecoes/Helper/OperacoesArray.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Colecoes.Helper
+{
+    internal class OperacoesArray
+    {
+        public void OrdenarBubbleSort(int[] array)
+        {
+            int temp = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array.Length - 1; j++)
+                {
+                    if(array[j] > array[j +1])
+                    {
+                        temp = array[j + 1];
+                        array[j + 1] = array[j];
+                        array[j] = temp;
+                    }
+                }
+            }
+        }
+
+        public void ImprimirArray(int[] array)
+        {
+            var linha = string.Join(", ", array);
+            Console.WriteLine(linha);
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    Console.WriteLine(array[i]);
+            //}
+        }
+    }
+}
--- Colecoes/Program.cs
+++ Colecoes/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Colecoes.Helper;
+using System;
 
 namespace Colecoes
 {
@@ -6,21 +7,33 @@ namespace Colecoes
     {
         static void Main(string[] args)
         {
-            int[] arrayInteiros = new int[3];
-            arrayInteiros[0] = 10;
-            arrayInteiros[1] = 20;
-            arrayInteiros[2] = 30;
-            //no for, o ideial é usar a propriedade lenght de um array para caso aconteça de algum momento mudar o tamanho do array, não terá impacto aqui no for
-            for (int i = 0; i < arrayInteiros.Length; i++)
-            {
-                Console.WriteLine(arrayInteiros[i]);
-            }
+            //int[] arrayInteiros = new int[3];
+            //arrayInteiros[0] = 10;
+            //arrayInteiros[1] = 20;
+            //arrayInteiros[2] = 30;
+            ////no for, o ideial é usar a propriedade lenght de um array para caso aconteça de algum momento mudar o tamanho do array, não terá impacto aqui no for
+            //for (int i = 0; i < arrayInteiros.Length; i++)
+            //{
+            //    Console.WriteLine("For " + arrayInteiros[i]);
+            //}
 
-            //tbm pode-se usar o for, sem a necessidade de um contador
-            foreach(int i in arrayInteiros)
-            {
-                Console.WriteLine("foreach" + i);
-            }
+            ////tbm pode-se usar o for, sem a necessidade de um contador
+            //foreach(int i in arrayInteiros)
+            //{
+            //    Console.WriteLine("Foreach " + i);
+            //}
+
+            //Ordenação de Arrays
+            OperacoesArray op = new OperacoesArray();
+            int[] array = new int[5] { 8, 7, 12, 141,23 };
+            Console.WriteLine("Array Inicial");
+            op.ImprimirArray(array);
+
+            op.OrdenarBubbleSort(array);
+
+            Console.WriteLine("Array Ordenado");
+            op.ImprimirArray(array);
+
         }
     }
 }
